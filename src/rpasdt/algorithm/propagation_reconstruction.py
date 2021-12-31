6cfed486598bd49e96282f664d96055963c934a3
Damian Frąszczak <dfraszczak@motoinsight.com>
2021-12-31 10:25:18 +0100

    plot different cases

--- src/rpasdt/algorithm/propagation_reconstruction.py
+++ src/rpasdt/algorithm/propagation_reconstruction.py
@@ -1,13 +1,18 @@
 import math
 import operator
 import random
-from functools import reduce, lru_cache
+from functools import lru_cache, reduce
 from typing import List, Set
 
+import matplotlib
+import matplotlib.pyplot as plt
 import networkx as nx
 from networkx import Graph
 
 from rpasdt.algorithm.models import PropagationReconstructionConfig
+
+matplotlib.use("Qt5Agg")
+
 
 NODE_INFECTION_PROBABILITY_ATTR = "INFECTION_PROBABILITY"
 WEIGHT_ATTR = "weight"
@@ -37,15 +42,28 @@ def _compute_edges_weights(EG: Graph):
 
 
 def _get_nodes_to_process(EG: Graph, threshold: float) -> List[int]:
-    return [
+    nodes = [
         node
         for node, data in EG.nodes(data=True)
         if data[NODE_INFECTION_PROBABILITY_ATTR] < threshold
     ]
 
+    return list(
+        sorted(
+            nodes,
+            key=lambda node: len(
+                [
+                    nn
+                    for nn in nx.neighbors(EG, node)
+                    if EG.nodes[node][NODE_INFECTION_PROBABILITY_ATTR] > threshold
+                ]
+            ),
+            reverse=True,
+        )
+    )
 
-def _check_node_in_external_network(node: int,
-                                    infected_nodes: Set[int]) -> bool:
+
+def _check_node_in_external_network(node: int, infected_nodes: Set[int]) -> bool:
     """
     Return True if node is detected to send a rumor in other site
     False otherwise.
@@ -58,11 +76,11 @@ def _check_node_in_external_network(node: int,
 
 def _compute_neighbors_probability(node: int, G: Graph) -> float:
     neighbors_probability = [
-        G.nodes[node][NODE_INFECTION_PROBABILITY_ATTR]
-        for node in nx.neighbors(G, node)
+        G.nodes[node][NODE_INFECTION_PROBABILITY_ATTR] for node in nx.neighbors(G, node)
     ]
     return reduce(
-        operator.mul, neighbors_probability,
+        operator.mul,
+        neighbors_probability,
         1,
     )
 
@@ -72,8 +90,7 @@ def _get_shortest_path(G: Graph, source: int, target: int) -> List[int]:
     return nx.shortest_path(G, source=source, target=target)
 
 
-def _check_if_node_is_on_path_between_infected_nodes(node: int,
-                                                     G: Graph) -> bool:
+def _check_if_node_is_on_path_between_infected_nodes(node: int, G: Graph) -> bool:
     neighbors = nx.neighbors(G, node)
     for n1 in neighbors:
         for n2 in neighbors:
@@ -90,10 +107,13 @@ def _compute_node_recovery(
 ) -> float:
     neighbors_probability = _compute_neighbors_probability(node=node, G=EG)
     node_on_path = int(
-        _check_if_node_is_on_path_between_infected_nodes(node=node, G=EG))
-    node_in_external_network = int(_check_node_in_external_network(
-        node=node, infected_nodes=config.real_infected_nodes
-    ))
+        _check_if_node_is_on_path_between_infected_nodes(node=node, G=EG)
+    )
+    node_in_external_network = int(
+        _check_node_in_external_network(
+            node=node, infected_nodes=config.real_infected_nodes
+        )
+    )
 
     m1 = config.m1 * neighbors_probability
     m2 = config.m2 * node_on_path
@@ -117,22 +137,16 @@ def reconstruct_propagation(config: PropagationReconstructionConfig) -> Graph:
 
     nodes = _get_nodes_to_process(EG, config.threshold)
     for node in nodes:
-        EG.nodes[node][
-            NODE_INFECTION_PROBABILITY_ATTR] = _compute_node_recovery(EG=EG,
-                                                                      node=node,
-                                                                      config=config)
+        EG.nodes[node][NODE_INFECTION_PROBABILITY_ATTR] = _compute_node_recovery(
+            EG=EG, node=node, config=config
+        )
     _compute_edges_weights(EG)
     _remove_invalid_edges_and_nodes(EG, config.threshold)
     return EG
 
 
 def plt_with_weight(G):
-    import matplotlib
-    import matplotlib.pyplot as plt
-
-    matplotlib.use("Qt5Agg")
-    pos = nx.spring_layout(G,
-                           seed=500)  # pos = nx.nx_agraph.graphviz_layout(G)
+    pos = nx.spring_layout(G, seed=500)  # pos = nx.nx_agraph.graphviz_layout(G)
     nx.draw_networkx(G, pos)
     labels = nx.get_edge_attributes(G, "weight")
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
@@ -143,18 +157,46 @@ def create_sample_IG(G, number_to_remove=None):
     IG = G.copy()
     if not number_to_remove:
         number_to_remove = math.ceil(len(G.nodes) / 10)
-    IG.remove_nodes_from(random.choices(list(G.nodes), k=number_to_remove))
-    return IG
+    to_remove = random.choices(list(G.nodes), k=number_to_remove)
+    IG.remove_nodes_from(to_remove)
+    return IG, to_remove
+
+
+def draw_results(G, RealIG, IG, EG, removed, extended):
+    fig, axes = plt.subplots(nrows=1, ncols=3)
+    axes[0].set_title("Full infection")
+    pos = nx.spring_layout(RealIG, seed=100)
+    nx.draw_networkx(
+        RealIG,
+        pos,
+        node_color=["red" if node in removed else "#1f78b4" for node in G],
+        ax=axes[0],
+    )
+    axes[1].set_title("Infection with loss")
+    nx.draw_networkx(IG, pos, ax=axes[1])
+    axes[2].set_title("Reconstructed infection")
+    nx.draw_networkx(
+        EG,
+        pos,
+        node_color=["green" if node in extended else "#1f78b4" for node in EG],
+        ax=axes[2],
+    )
+
+    plt.tight_layout()
+    plt.show()
 
 
 def main():
     G = nx.karate_club_graph()
     RealIG = G
-    IG = create_sample_IG(RealIG)
+    IG, removed = create_sample_IG(RealIG, number_to_remove=6)
     EG = reconstruct_propagation(
         PropagationReconstructionConfig(G=G, IG=IG, real_IG=RealIG)
     )
-    plt_with_weight(EG)
+    extended = EG.nodes - IG.nodes
+    print(f"Removed- {set(removed)}")
+    print(f"Extended - {set(extended)}")
+    draw_results(G, RealIG, IG, EG, removed, extended)
 
 
 if __name__ == "__main__":
