e3709269d480d1ff28f303de7217909a0f42d86b
Damian Frąszczak <dfraszczak@motoinsight.com>
2022-10-31 11:06:24 +0100

    return all nodes estimation instead of just best

--- src/rpasdt/algorithm/source_detectors/net_sleuth.py
+++ src/rpasdt/algorithm/source_detectors/net_sleuth.py
@@ -14,6 +14,7 @@ class NetSleuthCommunityBasedSourceDetector(CommunityBasedSourceDetector):
         L = nx.laplacian_matrix(graph).todense().A
         w, v = np.linalg.eig(L)
         v1 = v[np.where(w == np.min(w))][0]
-        max_val = np.max(v1)
-        sources = nodes[np.where(v1 == np.max(v1))]
-        return {source: max_val for source in sources}
+        # max_val = np.max(v1)
+        # sources = nodes[np.where(v1 == np.max(v1))]
+        # return {source: max_val for source in sources}
+        return {node: v1[node] for node in nodes}
